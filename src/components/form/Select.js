dbbb85f7878b023f3a80bd1b0cfc0afc7b7364f4
Pablo Barcellos Soares <pablobarcellossoares@gmail.com>
2024-08-15 15:51:49 -0300

    API pelo React

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -6,6 +6,11 @@ function Select({ text, name, options, handleOnChange, value }) {
       <label htmlFor={name}>{text}:</label>
       <select name={name} id={name} onChange={handleOnChange} value={value}>
         <option>Selecione uma opção</option>
+        {options.map((opt) => (
+          <option value={opt.id} key={opt.id}>
+            {opt.name}
+          </option>
+        ))}
       </select>
     </div>
   )
